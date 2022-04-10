c9574011c39d8fae64e139bd5e26e0f54f71ebb6
Martin Alexander Flores Torres <martinflorestorres21@gmail.com>
2022-04-10 17:06:48 -0500

    actualizaciones

--- app.js
+++ app.js
@@ -2,20 +2,21 @@
 const carousel = [...document.querySelectorAll(".carousel img")];
 
 let carouselImageIndex = 0;
-
-const changeCarousel = () => {
+changeCarousel();
+function changeCarousel() {
+    carousel.forEach((img) => {
+        img.classList.remove("active");
+    });
     carousel[carouselImageIndex].classList.toggle("active");
-
     if (carouselImageIndex >= carousel.length - 1) {
         carouselImageIndex = 0;
     } else {
-        carouselImageIndex++;
+        ++carouselImageIndex;
     }
-};
-
+}
 setInterval(() => {
     changeCarousel();
-}, 3000);
+}, 2000);
 
 //////////////////////////////navigation/////////////////////////
 
@@ -23,8 +24,9 @@ setInterval(() => {
 
 const musicPlayerSection = document.querySelector(".music-player-section");
 const botonArriba = document.querySelector(".botonArriba");
-    botonArriba.addEventListener("click", () => {
+botonArriba.addEventListener("click", () => {
     musicPlayerSection.classList.add("active");
+    document.querySelector("body").style.overflow = "hidden";
 });
 ///////////////////back from music player///////////////////////
 
@@ -32,6 +34,7 @@ const backToHomeBtn = document.querySelector(".music-player-section .back-btn");
 
 backToHomeBtn.addEventListener("click", () => {
     musicPlayerSection.classList.remove("active");
+    document.querySelector("body").style.overflow = "auto";
 });
 
 ///////////////////////////access play list///////////////////////
@@ -71,7 +74,9 @@ const retrocederBtn = document.querySelector("i.bx-rewind");
 const avanzarBtn = document.querySelector("i.bx-fast-forward");
 const playBtn = document.querySelector("i.bx-play");
 const pauseBtn = document.querySelector("i.bx-pause");
-const repetirdBtn = document.querySelector("span.bx-redo");
+const ramdomBtn = document.querySelector("span.bx-shuffle");
+const uno = document.querySelector("span .uno");
+const repostBtn = document.querySelector("span.bx-repost");
 const volumenBtn = document.querySelector("span.bxs-volume-full");
 const volumenSlider = document.querySelector(".volume-slider");
 
@@ -90,31 +95,7 @@ pauseBtn.addEventListener("click", () => {
     pauseBtn.classList.remove("active");
     playBtn.classList.add("active");
 });
-
-//// funcion for setting up music
-
-const setMusic = (i) => {
-    seekbar.value = 0;
-    let song = canciones[i];
-    currentMusic = i;
-
-    music.src = song.path;
-
-    songName.innerHTML = song.name;
-    artistName.innerHTML = song.artist;
-    coverImg.src = song.cover;
-
-    setTimeout(() => {
-        seekbar.max = music.duration;
-        musicaDuration.innerHTML = formaTime(music.duration);
-    }, 300);
-
-    musicaTime.innerHTML = "00 : 00";
-    queve.forEach((item) => item.classList.remove("active"));
-    queve[currentMusic].classList.add("active");
-};
-setMusic(0);
-
+/*  */
 //// for duracion de 00 : 00 formato
 
 const formaTime = (time) => {
@@ -132,20 +113,86 @@ const formaTime = (time) => {
 
     return `${min} : ${sec}`;
 };
+//// funcion for setting up music
 
+let song = {};
+const setMusic = (i) => {
+    seekbar.value = 0;
+    song = canciones[i];
+    currentMusic = i;
+    music.src = song.path;
+    songName.innerHTML = song.name;
+    artistName.innerHTML = song.artist;
+    coverImg.src = song.cover;
+    seekbar.max = music.duration;
+    musicaDuration.textContent = "00 : 00";
+    setTimeout(() => {
+        musicaDuration.textContent = formaTime(music.duration);
+    }, 5000);
+
+    musicaTime.innerHTML = "00 : 00";
+    queve.forEach((item) => item.classList.remove("active"));
+    queve[currentMusic].classList.add("active");
+};
+setMusic(0);
+document.addEventListener("DOMContentLoaded", () => {
+    let song2 = JSON.parse(localStorage.getItem("musica") || 0);
+    if (song2 != 0) {
+        music.src = song2.path;
+        songName.innerHTML = song2.name;
+        artistName.innerHTML = song2.artist;
+        coverImg.src = song2.cover;
+        seekbar.max = music.duration;
+
+        musicaDuration.textContent = "00 : 00";
+        queve.forEach((item, i) => {
+            item.classList.remove("active");
+            if (i == song2.id - 1) {
+                item.classList.add("active");
+            }
+        });
+    }
+});
+
+/*  */
+setInterval(() => {
+    seekbar.max = music.duration;
+    musicaDuration.textContent = formaTime(music.duration);
+}, 8000);
 /// evento de seekbar
 setInterval(() => {
     seekbar.value = music.currentTime;
-    musicaTime.innerHTML = formaTime(music.currentTime);
+    musicaTime.textContent = formaTime(music.currentTime);
 
-    if (Math.floor(music.currentTime) == Math.floor(seekbar.value)){
-
-        if (repetirdBtn.className.includes('active')) {    
+    if (Math.floor(music.currentTime) == Math.floor(seekbar.value)) {
+        if (repostBtn.className.includes("color")) {
             playBtn.click();
-        } 
+        }
+    }
+    if (seekbar.value == parseInt(seekbar.max)) {
+        if (repostBtn.className.includes("active")) {
+            if (currentMusic >= canciones.length - 1) {
+                currentMusic = 0;
+            } else {
+                currentMusic++;
+            }
+            setMusic(currentMusic);
+            playBtn.click();
+        }
+    }
+    if (seekbar.value == parseInt(seekbar.max)) {
+        if (ramdomBtn.className.includes("active")) {
+            if (currentMusic >= canciones.length - 1) {
+                currentMusic = 0;
+            } else {
+                let n = parseInt(Math.random() * 13);
+                currentMusic = n;
+            }
+            setMusic(currentMusic);
+            playBtn.click();
+        }
     }
 }, 500);
-
 seekbar.addEventListener("change", () => {
     music.currentTime = seekbar.value;
 });
@@ -156,9 +203,10 @@ retrocederBtn.addEventListener("click", () => {
     } else {
         currentMusic--;
     }
-
     setMusic(currentMusic);
     playBtn.click();
+    localStorage.setItem("musica", JSON.stringify(song));
+    musicaDuration.textContent = "00 : 00";
 });
 
 avanzarBtn.addEventListener("click", () => {
@@ -167,17 +215,40 @@ avanzarBtn.addEventListener("click", () => {
     } else {
         currentMusic++;
     }
+
     setMusic(currentMusic);
     playBtn.click();
+    localStorage.setItem("musica", JSON.stringify(song));
+    musicaDuration.textContent = "00 : 00";
 });
-
-
+let v = 0;
 ///// repetir
-
-repetirdBtn.addEventListener("click", () => {
-    repetirdBtn.classList.toggle("active");
+ramdomBtn.addEventListener("click", () => {
+    ramdomBtn.classList.toggle("active");
+    uno.classList.remove("replay");
+    repostBtn.classList.remove("active");
+    repostBtn.classList.remove("color");
 });
 
+repostBtn.addEventListener("click", () => {
+    ramdomBtn.classList.remove("active");
+    uno.classList.remove("replay");
+    repostBtn.classList.remove("color");
+    repostBtn.classList.add("active");
+
+    repostBtn.addEventListener("click", () => {
+        repostBtn.classList.toggle("active");
+        repostBtn.classList.add("color");
+        uno.classList.add("replay");
+        v++;
+        if (v == 3) {
+            v = 0;
+            repostBtn.classList.toggle("active");
+            uno.classList.toggle("replay");
+            repostBtn.classList.remove("color");
+        }
+    });
+});
 //// volumen
 volumenBtn.addEventListener("click", () => {
     volumenBtn.classList.toggle("active");
@@ -190,7 +261,10 @@ volumenSlider.addEventListener("input", () => {
 ////   playlist queve
 queve.forEach((item, i) => {
     item.addEventListener("click", () => {
+        playListSection.classList.remove("active");
         setMusic(i);
         playBtn.click();
+        localStorage.setItem("musica", JSON.stringify(song));
+        musicaDuration.textContent = "00 : 00";
     });
 });
--- data.js
+++ data.js
@@ -1,57 +1,93 @@
 let canciones = [
     {
-        name: 'Imposible Olvidar',
-        artist: 'Abraham mateo',
-        path: 'mp3/cancion1.mp3',
-        cover: 'img/imposible.jpg'
+        name: "Balada",
+        artist: "Gustavo Lima",
+        path: "mp3/c10.mp3",
+        cover: "https://m.media-amazon.com/images/I/61StS3JNysL._SX466_.jpg",
+        id : 1
     },
-
     {
-        name: 'Another Heartbreak',
-        artist: 'Abraham mateo',
-        path: 'mp3/cancion2.mp3',
-        cover: 'img/noter.jpg'
+        name: "Dueles",
+        artist: "Jesse - Joy",
+        path: "mp3/c6.mp3",
+        cover: "https://i.ytimg.com/vi/EEYHsvcig5I/mqdefault.jpg",
+        id : 2
     },
-
     {
-        name: 'Enamorate Otra Vez',
-        artist: 'Abraham mateo',
-        path: 'mp3/cancion3.mp3',
-        cover: 'img/ena.jpg'
+        name: "Hasta la Raíz",
+        artist: "Natalia Lafourcade",
+        path: "mp3/c7.mp3",
+        cover: "https://i.ytimg.com/vi/c7VK1LiG90Q/sddefault.jpg",
+        id : 3
     },
-
     {
-        name: 'Lo Siento',
-        artist: 'Beret',
-        path: 'mp3/cancion4.mp3',
-        cover: 'img/siento.jpg'
+        name: "drivers license",
+        artist: "Olivia Rodrigo",
+        path: "mp3/c8.mp3",
+        cover: "https://i.ytimg.com/vi/ZmDBbnmKpqQ/maxresdefault.jpg",
+        id : 4
     },
-
     {
-        name: 'La Estrategia',
-        artist: 'Cali y el dandee',
-        path: 'mp3/cancion5.mp3',
-        cover: 'img/estrategia.jpg'
+        name: "Unstoppable",
+        artist: "Sia",
+        path: "mp3/c9.mp3",
+        cover: "https://i1.sndcdn.com/artworks-000162499586-vifye7-t240x240.jpg",
+        id : 5
     },
-
     {
-        name: 'Te necesito',
-        artist: 'cali y el dandee',
-        path: 'mp3/cancion6.mp3',
-        cover: 'img/tenecesito.jpg'
+        name: "3MSC cancion final",
+        artist: "3MSC",
+        path: "mp3/c1.mp3",
+        cover: "https://i0.wp.com/instyle.mx/wp-content/uploads/2020/04/a-tres-sobre-el-cielo.jpg?fit=1280%2C1280&ssl=1",
+        id : 6
     },
-
     {
-        name: 'Que es el amor',
-        artist: 'Daniel y nacho',
-        path: 'mp3/cancion7.mp3',
-        cover: 'img/elamor.jpg'
+        name: "Toxic",
+        artist: "BoyWithUke",
+        path: "mp3/c2.mp3",
+        cover: "https://studiosol-a.akamaihd.net/uploadfile/letras/fotos/d/e/e/a/deea4bdb13aa38798b9ffa7739214b5b.jpg",
+        id : 7
     },
-
     {
-        name: 'Que tienes tu',
-        artist: 'Devicio',
-        path: 'mp3/cancion8.mp3',
-        cover: 'img/tienestu.jpg'
+        name: "Locked Out Of Heaven",
+        artist: "Bruno Mars",
+        path: "mp3/c3.mp3",
+        cover: "https://i1.sndcdn.com/artworks-000076266558-o9d8s4-t500x500.jpg",
+        id : 8
     },
-]
+    {
+        name: "Talking To The Moon",
+        artist: "Bruno Mars",
+        path: "mp3/c4.mp3",
+        cover: "https://i.ytimg.com/vi/CsKpw6ppj5I/maxresdefault.jpg",
+        id : 9
+    },
+    {
+        name: "Yellow ",
+        artist: "Coldplay",
+        path: "mp3/c5.mp3",
+        cover: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/5c5dc4664bf76a183b1087cc51e1bf74-1000x994x1-1534755950.jpg?crop=1xw:1xh;center,top&resize=480:*",
+        id : 10
+    },
+    {
+        name: "Another Heartbreak",
+        artist: "Abraham mateo",
+        path: "mp3/cancion2.mp3",
+        cover: "https://www.vagalume.com.br/abraham-mateo/images/abraham-mateo.jpg",
+        id : 11
+    },
+    {
+        name: "Enamorate Otra Vez",
+        artist: "Abraham mateo",
+        path: "mp3/cancion3.mp3",
+        cover: "https://i.ytimg.com/vi/15KKDLSpDzk/maxresdefault.jpg",
+        id : 12
+    },
+    {
+        name: "Que es el amor",
+        artist: "Daniel y nacho",
+        path: "mp3/cancion7.mp3",
+        cover: "https://www.quepasa.com.ve/site/wp-content/uploads/2018/04/NACHO222.png",
+        id : 13
+    },
+];
